e83b4f0aa9e697237f10b97d40b97815f17989b0
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-29 12:30:25 -0700

    Added tsConfigPaths plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {
     open: true, 
-  }
+  },
+  plugins: [tsconfigPaths()]
 });
