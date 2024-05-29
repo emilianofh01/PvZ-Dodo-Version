f28a6924ab9eb817ddfee001393743a5026c0e0a
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-29 12:31:58 -0700

    Made game class and changed loop logic to that

---
+++ src/game/game.ts
@@ -0,0 +1,22 @@
+import Dodo from "$/Dodo";
+import {Game} from "./scenes/Game.ts";
+
+export class PlantsVsZombies {
+    readonly dodo: Dodo;
+    
+    constructor(canvas: HTMLCanvasElement) {
+        this.dodo = new Dodo(canvas);
+    }
+    
+    loop = (time: number) => {
+        this.dodo.loop(time);
+        window.requestAnimationFrame(this.loop);
+    }
+    
+    run(){
+        this.dodo.transitionTo(e => new Game(e));
+        this.dodo.render();
+        this.loop(0)
+    }
+    
+}
