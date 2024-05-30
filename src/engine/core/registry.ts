1ea895dfd6c89d1fa2e54d0ac4de894329e4fceb
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-29 23:15:30 -0700

    Made registry class

---
+++ src/engine/core/registry.ts
@@ -0,0 +1,15 @@
+export class Registry<T> {
+    readonly registry_map = new Map<string, T>;
+
+    add(key: string, obj: T){
+        this.registry_map.set(key, obj);
+    }
+
+    get(key: string){
+        return this.registry_map.get(key);
+    }
+
+    getAll() : IterableIterator<T> {
+        return this.registry_map.values();
+    }
+}
