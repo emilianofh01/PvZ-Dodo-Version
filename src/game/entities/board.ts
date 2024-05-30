48b9b55eea540e1a0cb9df3b079834c0d9924318
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-30 03:28:30 -0700

    Highlighted selected cell on board

--- src/game/entities/board.ts
+++ src/game/entities/board.ts
@@ -7,15 +7,27 @@ export class GameBoard implements Entity {
     readonly position: [number, number];
     readonly cell_size: [number, number];
     readonly board_size: [number, number];
+    highlightedCell: [number, number] | null = null;
     
+    get boundingBox(): [number, number, number, number] {
+        return [...this.position, this.cell_size[0] * this.board_size[0], this.cell_size[0] * this.board_size[0]];
+    }
+
     constructor(_: Scene) {
         this.cell_size = [32, 32]; 
         this.board_size = [12, 5];
         this.position = [0, 80];
     }
     
-    get boundingBox(): [number, number, number, number] {
-        return [...this.position, this.cell_size[0] * this.board_size[0], this.cell_size[0] * this.board_size[0]];
+    mousePosUpdate(pos: [number, number]){
+        this.highlightedCell = [
+            Math.floor(pos[0] / this.cell_size[0]),
+            Math.floor(pos[1] / this.cell_size[1])
+        ];
+    }
+
+    placePlant(pos: [number, number]) {
+        
     }
     
     tick(_: number) : void {
@@ -26,9 +38,12 @@ export class GameBoard implements Entity {
         for(let i = 0; i < this.board_size[0]; i++) {
             for(let j = 0; j < this.board_size[1]; j++) {
                 renderer.context.renderRect((i + j) % 2 ? "#000" : "#fff", this.position[0] + this.cell_size[0] * i, this.position[1] + this.cell_size[1] * j, ...this.cell_size)
+                if(!this.highlightedCell || this.highlightedCell[0] != i || this.highlightedCell[1] != j) continue;
+                renderer.context.renderRect("#aaa", this.position[0] + this.cell_size[0] * i, this.position[1] + this.cell_size[1] * j, ...this.cell_size)
             }
         }
     }
+
     dispose(): void{
         
     }
