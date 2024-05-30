cf6408920de63dbdfcb114ac6b45fe5a1a7c3239
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-29 23:23:53 -0700

    Switched to sun counter entity and added the card holder entity

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,14 +2,23 @@ import {Scene} from "$/scene/Scene.ts";
 import Dodo from "$/Dodo.ts";
 import {SunCounter} from "../overlays/SunCounter.ts";
 import {GameBoard} from "../entities/board.ts";
+import { SunEntity } from "src/entities/SunEntity.ts";
+import { CardHolder } from "../entities/card_holder.ts";
 
 export class Game extends Scene {
     currentSun= 15;
     
     constructor(dodo: Dodo) {
         super(dodo);
-        this.dodo.guiController.addOverlay(new SunCounter(this.getSun));
+        this.addEntity(_ => new SunCounter(this.getSun));
         this.addEntity(s => new GameBoard(s))
+        this.addEntity(s => new SunEntity({
+            degreesPerSecond: 90,
+            position: [90, 90],
+            size: [32, 32],
+            sunAmount: 90
+        }, s.dodo))
+        this.addEntity(e => new CardHolder())
     }
     
     
