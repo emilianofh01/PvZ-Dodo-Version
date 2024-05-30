e2befe48866931bf58d989874454e94589b327e9
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-29 23:17:18 -0700

    Changed path of import

--- src/engine/rendering/Renderer.ts
+++ src/engine/rendering/Renderer.ts
@@ -1,6 +1,6 @@
 import IScene from "../scene/IScene";
 import { notNull } from "../../utils/Objects";
-import { IGUIController } from "../../gui/controller";
+import { IGUIController } from "$/gui/controller";
 
 declare global {
     interface CanvasRenderingContext2D extends CanvasRenderingContext2DEx {}
