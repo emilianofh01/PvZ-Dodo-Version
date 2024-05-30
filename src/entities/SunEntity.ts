9fd16e24c91411c231b131204cb39a5381ffef14
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-29 23:22:34 -0700

    Sun entity now adds sun on click and takes the sun amount on their props

--- src/entities/SunEntity.ts
+++ src/entities/SunEntity.ts
@@ -3,11 +3,15 @@ import Dodo from "../engine/Dodo.ts";
 import ResourceManagement from "../engine/resource_management/ResourceManager.ts";
 import {ASSET_TYPES, AssetKey} from "../engine/resource_management/IResourceLoader.ts";
 import Renderer, {PIVOTS} from "../engine/rendering/Renderer.ts";
+import { MOUSE, MouseButton, MouseEventData } from "$/input/mouse.ts";
+import { point2Rect } from "$/core/collision.ts";
+import { Game } from "src/game/scenes/Game.ts";
 
 export interface SunProperties {
     degreesPerSecond: number
     position: [number, number]
     size: [number, number]
+    sunAmount: number
 }
 
 export class SunEntity<T extends SunProperties = SunProperties> implements Entity {
@@ -18,6 +22,7 @@ export class SunEntity<T extends SunProperties = SunProperties> implements Entit
     scale: number = 1;
     
     _boundingBox: [number,number,number,number];
+    _boundingBoxWPivot: [number,number,number,number];
     
     get boundingBox () {
         return this._boundingBox;
@@ -34,6 +39,8 @@ export class SunEntity<T extends SunProperties = SunProperties> implements Entit
             this.sunSprite = e;
         });
         this._boundingBox = [...props.position, ...props.size];
+        this._boundingBoxWPivot = [props.position[0] - props.size[0] * .5, props.position[1] - props.size[1] * .5, ...props.size];
+        this.dodo.listener_manager.addEventListener(MOUSE, this.on_click)
     }
     
     tick(delta: number){
@@ -46,11 +53,21 @@ export class SunEntity<T extends SunProperties = SunProperties> implements Entit
     
     draw(renderer: Renderer) {
         if(!this.sunSprite) return;
-        const scales = [1, 1, this.scale, this.scale];
-        renderer.context.drawImageRotated(this.sunSprite, this.rotation / 1000 * Math.PI / 180, PIVOTS.MID_CENTER, ...this.boundingBox.map((e, i) => e * scales[i]));
+        renderer.context.renderRect("#f00",...this._boundingBoxWPivot);
+        renderer.context.drawImageRotated(this.sunSprite, this.rotation / 1000 * Math.PI / 180, PIVOTS.MID_CENTER, this.boundingBox[0], this.boundingBox[1], this.boundingBox[2] * this.scale, this.boundingBox[3] * this.scale);
     }
-    
+
+    on_click = (event: MouseEventData) => {
+        if(event.button !== MouseButton.Primary) return;
+        if(!point2Rect(event.position, this._boundingBoxWPivot)) return;
+        if(this.dodo.currentScene instanceof Game){
+            this.dodo.currentScene.currentSun += this.properties.sunAmount;
+            this.dodo.currentScene.removeEntity(this);
+        }
+    }
+
     dispose(): void{
         this.sunSprite = null;
+        this.dodo.listener_manager.removeEventListener(MOUSE, this.on_click)
     }
 }
