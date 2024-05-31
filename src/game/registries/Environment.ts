8e4f335806cb424feb94c48bc4b6359f49db073d
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-31 02:33:34 -0700

    Added environments registry, and sunny environment

---
+++ src/game/registries/Environment.ts
@@ -0,0 +1,32 @@
+import Entity from "src/entities/Entity";
+import { Game } from "../scenes/Game";
+import { Registry } from "$/core/registry";
+import { SunnyEnvironment } from "../entities/environments/sunny";
+import { Scene } from "$/scene/Scene";
+import { SunEntity } from "src/entities/SunEntity";
+
+export interface EnvironmentEntry {
+    isSunny: boolean;
+    factory: (game: Game) => Entity;
+}
+
+export const ENVIRONMENTS_REGISTRY = new Registry<EnvironmentEntry>();
+
+ENVIRONMENTS_REGISTRY.add("dodo:sunny", {
+    isSunny: true,
+    factory: game => new SunnyEnvironment(
+        game, 
+        (startPosition: [number, number], endPosition: [number, number], duration: number, lifetime: number, sunAmount: number, scene: Scene) => new SunEntity(
+            {
+                degreesPerSecond: 90,
+                position: startPosition,
+                startPosition, 
+                endPosition,
+                size: [32, 32],
+                sunAmount: 50,
+                transitionDutarion: 10000
+            },
+            scene.dodo
+        )
+    )
+})
