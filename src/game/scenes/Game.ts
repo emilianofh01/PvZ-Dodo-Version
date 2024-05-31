37e3fb36ec648f6912a71b57a74fd37c743ebc43
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-31 06:22:44 -0700

    Removed test sun entity

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -8,22 +8,13 @@ import { ENVIRONMENTS_REGISTRY, EnvironmentEntry } from "../registries/Environme
 import { notNullOrUndefined } from "src/utils/Objects.ts";
 
 export class Game extends Scene {
-    currentSun = 15;
+    currentSun = 50;
     environment: EnvironmentEntry = notNullOrUndefined(ENVIRONMENTS_REGISTRY.get('dodo:sunny'));
     
     constructor(dodo: Dodo) {
         super(dodo);
         this.addEntity(_ => new SunCounter(this.getSun));
         const board = this.addEntity(s => new GameBoard(s))
-        this.addEntity(s => new SunEntity({
-            degreesPerSecond: 90,
-            position: [90, 90],
-            size: [32, 32],
-            sunAmount: 90,
-            startPosition: [90, 90],
-            endPosition: [200, 200],
-            transitionDutarion: 10000
-        }, s.dodo))
         this.addEntity(scene => this.environment.factory(scene as Game))
         this.addEntity(scene => new CardHolder(scene, board))
     }
