e79999b1792438bbd13ce9903a177abd1f7b2075
Christian Duarte <w.christian.duarte@outlook.com>
2024-05-31 02:33:43 -0700

    Added environment on game

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -4,9 +4,12 @@ import {SunCounter} from "../overlays/SunCounter.ts";
 import {GameBoard} from "../entities/board.ts";
 import { SunEntity } from "src/entities/SunEntity.ts";
 import { CardHolder } from "../entities/card_holder.ts";
+import { ENVIRONMENTS_REGISTRY, EnvironmentEntry } from "../registries/Environment.ts";
+import { notNullOrUndefined } from "src/utils/Objects.ts";
 
 export class Game extends Scene {
-    currentSun= 15;
+    currentSun = 15;
+    environment: EnvironmentEntry = notNullOrUndefined(ENVIRONMENTS_REGISTRY.get('dodo:sunny'));
     
     constructor(dodo: Dodo) {
         super(dodo);
@@ -16,8 +19,12 @@ export class Game extends Scene {
             degreesPerSecond: 90,
             position: [90, 90],
             size: [32, 32],
-            sunAmount: 90
+            sunAmount: 90,
+            startPosition: [90, 90],
+            endPosition: [200, 200],
+            transitionDutarion: 10000
         }, s.dodo))
+        this.addEntity(scene => this.environment.factory(scene as Game))
         this.addEntity(scene => new CardHolder(scene, board))
     }
     
