dc8007c4b7dd27d1fd7ccaaa22d3383bdef3d52a
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-04 00:21:30 -0700

    Added gameboard to the Game scene

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,7 +2,6 @@ import {Scene} from "$/scene/Scene.ts";
 import Dodo from "$/Dodo.ts";
 import {SunCounter} from "../overlays/SunCounter.ts";
 import {GameBoard} from "../entities/board.ts";
-import { SunEntity } from "src/entities/SunEntity.ts";
 import { CardHolder } from "../entities/card_holder.ts";
 import { ENVIRONMENTS_REGISTRY, EnvironmentEntry } from "../registries/Environment.ts";
 import { notNullOrUndefined } from "src/utils/Objects.ts";
@@ -10,11 +9,11 @@ import { notNullOrUndefined } from "src/utils/Objects.ts";
 export class Game extends Scene {
     currentSun = 50;
     environment: EnvironmentEntry = notNullOrUndefined(ENVIRONMENTS_REGISTRY.get('dodo:sunny'));
-    
+    gameBoard: GameBoard;
     constructor(dodo: Dodo) {
         super(dodo);
         this.addEntity(_ => new SunCounter(this.getSun));
-        const board = this.addEntity(s => new GameBoard(s))
+        const board = this.gameBoard = this.addEntity(s => new GameBoard(s))
         this.addEntity(scene => this.environment.factory(scene as Game))
         this.addEntity(scene => new CardHolder(scene, board))
     }
