cb158392d9a713f869bbc2fa040f04e1c8e05a90
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-05 21:00:32 -0700

    Added font setting and text rendering functions

--- src/engine/rendering/Renderer.ts
+++ src/engine/rendering/Renderer.ts
@@ -9,10 +9,82 @@ declare global {
 export type BackdropFill = string | CanvasGradient | CanvasPattern;
 export type Rect = [number, number, number, number];
 
+const BASELINES: CanvasTextBaseline[] = [
+    'alphabetic',
+    'bottom',
+    'hanging',
+    'ideographic',
+    'middle',
+    'top',
+];
+
+const ALIGNMENTS: CanvasTextAlign[] = [
+    'center',
+    'end',
+    'left',
+    'right',
+    'start',
+];
+
+export enum Baseline {
+    Alphabetic = 0,
+    Bottom = 1,
+    Hanging = 2,
+    Ideographic = 3, 
+    Middle = 4,
+    Top = 5,
+}
+
+export enum Alignment {
+    Center = 0,
+    End = 1,
+    Left = 2,
+    Right = 3, 
+    Start = 4,
+}
+
+interface FontStyle {
+    type: 'normal' | 'italic' | 'oblique'
+    rot?: number
+}
+
+interface DefaultFontStyles {
+    NORMAL: FontStyle
+    ITALIC: FontStyle
+    OBLIQUE: FontStyle
+}
+
+const FONT_STYLES : DefaultFontStyles = {
+    NORMAL: { type: 'normal' },
+    ITALIC: { type: 'italic' },
+    OBLIQUE: { type: 'oblique' },
+};
+
+interface FontWeight {
+    weight: 'normal' | 'bold' | 'bolder' | 'lighter' | 'absolute',
+    absolute?: number
+}
+
+interface DefaultFontWeights {
+    NORMAL: FontWeight
+    BOLD: FontWeight
+    BOLDER: FontWeight
+    LIGHTER: FontWeight
+}
+
+const FONT_WEIGHTS : DefaultFontWeights = {
+    NORMAL: { weight: 'normal' },
+    BOLD: { weight: 'bold' },
+    BOLDER: { weight: 'bolder' },
+    LIGHTER: { weight: 'lighter' },
+};
+
 export interface CanvasRenderingContext2DEx {
     clear: (color?: BackdropFill) => void
     renderRect: (fill: BackdropFill, ...rect: Rect) => void
     drawImageRotated: (image: CanvasImageSource, rotation: number, pivot: [number, number], sx: number, sy: number, sw: number, sh: number, dx?: number, dy?: number, dw?: number, dh?: number) => void
+    renderText: (x: number, y: number, text: string, baseline?: Baseline, alignment?: Alignment) => void;
+    setFont: (fontName: string, size: number, style?: FontStyle, weight?: FontWeight, lineHeight?: number) => void;
 }
 
 export const PIVOTS = Object.freeze({
@@ -35,6 +107,16 @@ CanvasRenderingContext2D.prototype.drawImageRotated = function (image: CanvasIma
     this.rotate(rotation);
     if (dx !== undefined && dy !== undefined && dw !== undefined && dh !== undefined) { this.drawImage(image, sx, sy, sw, sh, -dw * pivot[0], -dh * pivot[1], dw, dh); } else { this.drawImage(image, -sw * pivot[0], -sh * pivot[1], sw, sh); }
     this.restore();
+};
+
+CanvasRenderingContext2D.prototype.renderText = function (x: number, y: number, text: string, baseline: Baseline = Baseline.Middle, alignment: Alignment = Alignment.Center) {
+    this.textAlign = ALIGNMENTS[alignment];
+    this.textBaseline = BASELINES[baseline];
+    this.fillText(text, x, y);
+};
+
+CanvasRenderingContext2D.prototype.setFont = function (fontName: string, size: number, style: FontStyle = FONT_STYLES.NORMAL, weight: FontWeight = FONT_WEIGHTS.NORMAL, lineHeight?: number) {
+    this.font = `${style.type} ${style.type == 'oblique' ? style.rot ?? '' : ''} ${weight.absolute ?? weight.weight} ${size}px ${lineHeight ?? ''} ${fontName}`;
 };
 
 CanvasRenderingContext2D.prototype.clear = function (color) {
