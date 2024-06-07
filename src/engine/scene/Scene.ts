31264dac5a9288c507896a26859d7384b366d12e
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-07 01:55:25 -0700

    Doing rmv/Adding entities before every update

--- src/engine/scene/Scene.ts
+++ src/engine/scene/Scene.ts
@@ -5,10 +5,14 @@ import ResourceManagement from '../resource_management/ResourceManager';
 import { ASSET_TYPES, AssetKey } from '../resource_management/IResourceLoader';
 import Entity from '../../entities/Entity';
 import { PriorityQueue } from '@datastructures-js/priority-queue';
-import { queueIterable } from '$/core/priorityQueue';
+import { fixQueue, queueIterable } from '$/core/priorityQueue';
 
 export class Scene implements IScene {
     dodo: Dodo;
+
+    entitiesToRemove: Set<Entity> = new Set();
+
+    entitiesToAdd: Set<Entity> = new Set();
 
     entities: PriorityQueue<Entity>;
 
@@ -16,7 +20,7 @@ export class Scene implements IScene {
 
     constructor(dodo: Dodo) {
         this.dodo = dodo;
-        this.entities = new PriorityQueue<Entity>((a, b) => a.zIndex > b.zIndex ? 1 : a.zIndex < b.zIndex ? -1 : 0);
+        this.entities = new PriorityQueue<Entity>((a, b) => a.zIndex - b.zIndex);
     }
 
     dispose(): void {
@@ -29,18 +33,32 @@ export class Scene implements IScene {
 
     addEntity<T extends Entity>(provider: (scene: Scene) => T) {
         const entity = provider(this);
-        this.entities.push(entity);
+        this.entitiesToAdd.add(entity);
         return entity;
     }
 
     removeEntity(entity: Entity) {
         entity.dispose();
-        this.entities.remove(e => e == entity);
+        this.entitiesToRemove.add(entity);
     }
 
     update(delta: number): void {
+        if (this.entitiesToRemove.size > 0 || this.entitiesToAdd.size > 0) {
+            if (this.entitiesToRemove.size > 0) {
+                this.entities.remove(e => this.entitiesToRemove.has(e));
+                this.entitiesToRemove.clear();
+            }
+    
+            if (this.entitiesToAdd.size > 0) {
+                this.entitiesToAdd.forEach(e => this.entities.enqueue(e));
+                this.entitiesToAdd.clear();
+            }
+            fixQueue(this.entities);
+        }
+        
         for (const entity of queueIterable(this.entities)) {
-            entity.tick(delta);
+            if (!this.entitiesToRemove.has(entity))
+                entity.tick(delta);
         }
     }
 
