22ca6b3b76b13c8b327f4e34467f1b355cdabca8
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-07 01:54:29 -0700

    Refactored and added functions to get LivingEntity status

--- src/entities/LivingEntity.ts
+++ src/entities/LivingEntity.ts
@@ -19,20 +19,41 @@ export abstract class LivingEntity<T extends LivingEntityProps> implements Entit
         this.currentHealth = props.health;
     }
 
-    protected hasTakenDamage(_damager: LivingEntity<any>, _damageType: string, _amount: number) { }
+    get health() {
+        return this.currentHealth;
+    }
 
-    protected hasDoneDamage(_damagee: LivingEntity<any>, _damageType: string, _amount: number) { }
+    get dead() {
+        return this.currentHealth <= 0;
+    }
+
+    get alive() {
+        return this.currentHealth > 0;
+    }
+
+
+    protected onTakeDamage(_damager: LivingEntity<any>, _damageType: string, _amount: number) { }
+
+    protected onDeath(_damager: LivingEntity<any>, _damageType: string, _amount: number) { }
+
+    protected onDamage(_damagee: LivingEntity<any>, _damageType: string, _amount: number) { }
 
     makeDamage(damagee: LivingEntity<any>, damageType: string, amount: number) {
         amount = damagee.damageCalculation(this, damageType, amount);
-        this.hasDoneDamage(damagee, damageType, amount);
+        this.onDamage(damagee, damageType, amount);
     }
 
     takeDamage(damager: LivingEntity<any>, damageType: string, amount: number) {
+        if (this.currentHealth <= 0) {
+            return;
+        }
         amount = this.damageCalculation(damager, damageType, amount);
         if (amount > 0) {
             this.currentHealth -= amount;
-            this.hasTakenDamage(damager, damageType, amount);
+            this.onTakeDamage(damager, damageType, amount);
+            if (this.currentHealth <= 0) {
+                this.onDeath(damager, damageType, amount);
+            }
         }
     }
 
