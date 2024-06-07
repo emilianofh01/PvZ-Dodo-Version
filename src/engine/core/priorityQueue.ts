2b965bb747aba3fef42afd866850868c4b0906e8
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-07 01:47:47 -0700

    Added functions to sort the queue

--- src/engine/core/priorityQueue.ts
+++ src/engine/core/priorityQueue.ts
@@ -1,5 +1,13 @@
-import { PriorityQueue } from '@datastructures-js/priority-queue';
+import { MinPriorityQueue, PriorityQueue } from '@datastructures-js/priority-queue';
+
+export function fixQueue<T>(queue: PriorityQueue<T>) {
+    (((queue as any)._heap as any)._nodes as T[]).sort((queue as any)._heap._compare);
+}
 
 export function queueIterable <T>(queue: PriorityQueue<T>) {
     return ((queue as any)._heap as any)._nodes as T[];
+}
+
+export function minQueueIterable <T>(queue: MinPriorityQueue<T>) {
+    return ((queue as any)._heap._heap as any)._nodes as T[];
 }
