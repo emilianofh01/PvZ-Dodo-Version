34001bf6704c8acbb70c86e6f5c9528e0011d2d6
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-10 21:28:38 -0700

    Moved sprites to a different folders

--- src/game/registries/SpriteSheets.ts
+++ src/game/registries/SpriteSheets.ts
@@ -6,32 +6,32 @@ const SPRITESHEETS_REGISTRY = new Registry<SpriteSheet>();
 
 SPRITESHEETS_REGISTRY.add(
     'dodo:sunflower_idle', 
-    SpriteSheet.singleGroup(loadImage('./assets/img/sunflower_idle.png'), { grid_size: [2, 3], frames: 6 }),
+    SpriteSheet.singleGroup(loadImage('./assets/img/entities/plants/sunflower_idle.png'), { grid_size: [2, 3], frames: 6 }),
 );
 
 SPRITESHEETS_REGISTRY.add(
     'dodo:sunflower_glow',
-    SpriteSheet.singleGroup(loadImage('./assets/img/sunflower_glow.png'), { grid_size: [3, 3], frames: 6 }),
+    SpriteSheet.singleGroup(loadImage('./assets/img/entities/plants/sunflower_glow.png'), { grid_size: [3, 3], frames: 6 }),
 );
 
 SPRITESHEETS_REGISTRY.add(
     'dodo:peashooter_idle',
-    SpriteSheet.singleGroup(loadImage('./assets/img/peashooter_idle.png'), { grid_size: [3, 4], frames: 10 }),
+    SpriteSheet.singleGroup(loadImage('./assets/img/entities/plants/peashooter_idle.png'), { grid_size: [3, 4], frames: 10 }),
 );
 
 SPRITESHEETS_REGISTRY.add(
     'dodo:peashooter_shooting',
-    SpriteSheet.singleGroup(loadImage('./assets/img/peashooter_shooting.png'), { grid_size: [3, 3], frames: 9 }),
+    SpriteSheet.singleGroup(loadImage('./assets/img/entities/plants/peashooter_shooting.png'), { grid_size: [3, 3], frames: 9 }),
 );
 
 SPRITESHEETS_REGISTRY.add(
     'dodo:zombie_walking',
-    SpriteSheet.singleGroup(loadImage('./assets/img/zombie_walking.png'), { grid_size: [3, 3], frames: 8 }),
+    SpriteSheet.singleGroup(loadImage('./assets/img/entities/zombies/zombie_walking.png'), { grid_size: [3, 3], frames: 8 }),
 );
 
 SPRITESHEETS_REGISTRY.add(
     'dodo:pea',
-    SpriteSheet.singleGroup(loadImage('./assets/img/pea.png'), { grid_size: [1, 1], frames: 1 }),
+    SpriteSheet.singleGroup(loadImage('./assets/img/entities/projectiles/pea.png'), { grid_size: [1, 1], frames: 1 }),
 );
 
 SPRITESHEETS_REGISTRY.add(
