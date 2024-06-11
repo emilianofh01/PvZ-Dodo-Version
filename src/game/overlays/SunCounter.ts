546345e03a79482dfb632ac68e3e5b9487f2412f
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-11 02:26:53 -0700

    Drawing counter sprite

--- src/game/overlays/SunCounter.ts
+++ src/game/overlays/SunCounter.ts
@@ -1,7 +1,8 @@
-import Renderer, { PIVOTS } from '$/rendering/Renderer.ts';
+import Renderer, { Alignment, Baseline, PIVOTS } from '$/rendering/Renderer.ts';
 import { ASSET_TYPES, AssetKey } from '$/resource_management/IResourceLoader';
-import ResourceManagement from '$/resource_management/ResourceManager';
+import ResourceManagement, { loadImage } from '$/resource_management/ResourceManager';
 import Entity from 'src/entities/Entity';
+import { PromiseWaiter, notNull } from 'src/utils/Objects';
 
 export class SunCounter implements Entity {
     rotation: number = 0;
@@ -18,6 +19,8 @@ export class SunCounter implements Entity {
 
     readonly sunGetter: () => number;
 
+    readonly image = new PromiseWaiter(loadImage('./assets/img/gui/sun_counter.png'));
+
     constructor(sunGetter: () => number) {
         this.sunGetter = sunGetter;
         ResourceManagement.instance.load(new AssetKey(ASSET_TYPES.IMAGE, './assets/img/sun.png')).then(e => {
@@ -26,15 +29,12 @@ export class SunCounter implements Entity {
     }
 
     draw(renderer: Renderer): void {
-    // renderer.context.renderRect("#fff", ...[0, 0, renderer.context.canvas.width, 64+16]);
-        renderer.context.renderRect('#666', ...[8, 8, 48, 64]);
-
-        renderer.context.textAlign = 'center';
-        renderer.context.textBaseline = 'bottom';
+        if (this.image.get()) {
+            renderer.context.drawImage(notNull(this.image.get()), 8, 8);
+        }
         renderer.context.fillStyle = '#000';
-        renderer.context.font = '16px pixel';
-        renderer.context.textRendering = 'geometricPrecision';
-        renderer.context.fillText(this.sunGetter().toFixed(), 32, 70);
+        renderer.context.setFont('pixel', 16);
+        renderer.context.renderText(32, 69, this.sunGetter().toFixed(), Baseline.Alphabetic, Alignment.Center);
 
         if (this.sunSprite == null) return;
         renderer.context.drawImageRotated(this.sunSprite, this.rotation / 1000 * Math.PI / 180, PIVOTS.MID_CENTER, 24 + 8, 24 + 8, 32 * this.scale, 32 * this.scale);
