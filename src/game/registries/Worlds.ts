dfd08c91374dc978ebe5d38cb60172b278c064d1
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-11 10:54:23 -0700

    New: Worlds registry

---
+++ src/game/registries/Worlds.ts
@@ -0,0 +1,21 @@
+import { Registry } from '$/core/registry';
+import { notNullOrUndefined } from 'src/utils/Objects';
+import { ENVIRONMENTS_REGISTRY, EnvironmentEntry } from './Environment';
+import { LEVELS_REGISTRY, Level } from './Levels';
+
+export interface World {
+    name: String
+    environment: EnvironmentEntry
+    levels: Level[]
+}
+
+const WORLDS_REGISTRY = new Registry<World>();
+
+WORLDS_REGISTRY.add('dodo:day', {
+    name: 'Day',
+    environment: notNullOrUndefined(ENVIRONMENTS_REGISTRY.get('dodo:sunny')),
+    levels: [
+        notNullOrUndefined(LEVELS_REGISTRY.get('dodo:level_1-1')),
+        notNullOrUndefined(LEVELS_REGISTRY.get('dodo:level_1-2')),
+    ],
+});
