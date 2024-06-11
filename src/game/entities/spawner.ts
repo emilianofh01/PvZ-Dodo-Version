946b9928a3653eda7d7188031d319dcf82b6f05a
Christian Duarte <w.christian.duarte@outlook.com>
2024-06-11 10:51:48 -0700

    Added spawning start conditions

--- src/game/entities/spawner.ts
+++ src/game/entities/spawner.ts
@@ -1,9 +1,10 @@
 import Renderer from '$/rendering/Renderer';
 import { Scene } from '$/scene/Scene';
 import Entity from 'src/entities/Entity';
-import { GameBoard } from './board';
+import { GameBoard, PlantPlacementData } from './board';
 import { PriorityQueue } from '@datastructures-js/priority-queue';
-import { Level, Wave } from '../registries/Levels';
+import { Level, StartLevelCondition, Wave } from '../registries/Levels';
+import { Game } from '../scenes/Game';
 
 export class Spawner implements Entity {
     readonly zIndex: number = 100;
@@ -23,21 +24,65 @@ export class Spawner implements Entity {
 
     level?: Level;
 
+    hasStarted: boolean = false;
+
     time: number = 0;
+
+    conditions?: StartLevelCondition[];
+
+    doneSpawning: boolean = false;
 
     constructor(scene: Scene, board: GameBoard, start_x: number) {
         this.scene = scene;
         this.board = board;
         this.start_x = start_x;
+        this.board.addEventListener('plantplaced', this.plantPlaced);
+    }
+
+    plantPlaced = (data: CustomEvent<PlantPlacementData>) => {
+        console.log('a');
+        if (this.conditions) {
+            let changed = false;
+            for (const condition of this.conditions) {
+                changed = changed || condition.onPlant(data.detail);
+            }
+            if (changed) this.checkConditions();
+        }
+    };
+
+    checkConditions() {
+        if (this.conditions) {
+            let passed = true;
+            for (const condition of this.conditions) {
+                if (!condition.check(this.scene as Game)) {
+                    passed = false;
+                    break;
+                }
+            }
+            if (passed) {
+                this.hasStarted = true;
+            }
+        }
     }
 
     loadLevel(level: Level) {
         this.level = level;
         this.wavesQueue = new PriorityQueue((a, b) => a.time - b.time, level.waves);
+        this.conditions = level.startLevelConditions ?? [];
+        if (!level.startLevelConditions || level.startLevelConditions.length == 0) {
+            this.hasStarted = true;
+        }
     }
 
     tick(delta: number): void {
-        if (!this.wavesQueue || !this.level || this.wavesQueue.size() == 0) return;
+        if (!this.hasStarted && this.conditions) {
+            let changed = false;
+            for (const condition of this.conditions) {
+                changed = changed || condition.tick(delta);
+            }
+            if (changed) this.checkConditions();
+        }
+        if (!this.wavesQueue || !this.level || this.wavesQueue.size() == 0 || !this.hasStarted) return;
         let x = this.start_x;
         while (this.wavesQueue.front() && this.wavesQueue.front().time <= this.time) {
             const wave = this.wavesQueue.dequeue();
@@ -51,6 +96,9 @@ export class Spawner implements Entity {
                     position,
                 }, scene));
             }
+        }
+        if (this.wavesQueue.size() == 0) {
+            this.doneSpawning = true;
         }
         this.time += delta;
     }
--- src/game/registries/Levels.ts
+++ src/game/registries/Levels.ts
@@ -1,5 +1,8 @@
 import { Registry } from '$/core/registry';
 import { Scene } from '$/scene/Scene';
+import { randomInt } from 'src/utils/random';
+import { PlantPlacementData } from '../entities/board';
+import { Game } from '../scenes/Game';
 import PLANTS_REGISTRY, { PlantEntry } from './Plants';
 import ZOMBIES_REGISTRY, { ZombieEntry } from './Zombies';
 import { notNullOrUndefined } from 'src/utils/Objects';
@@ -9,10 +12,94 @@ export interface ZombieGeneration {
     selectLane: (scene: Scene) => number;
 }
 
+export interface StartLevelCondition {
+    readonly condition: string;
+    check(scene: Game): boolean;
+    onPlant(event: PlantPlacementData): boolean;
+    tick(delay: number): boolean;
+}
+
+class PlacePlantCondition implements StartLevelCondition {
+    readonly condition: string = 'dodo:place_plant';
+
+    readonly ammountToPlace: number;
+
+    readonly plantToPlace: PlantEntry;
+
+    private plantsPlaced = 0;
+
+    constructor(plant: PlantEntry, plantsToPlace: number = 1) {
+        this.ammountToPlace = plantsToPlace;
+        this.plantToPlace = plant;
+    }
+
+    check(_scene: Game): boolean {
+        return this.plantsPlaced >= this.ammountToPlace;
+    }
+
+    onPlant(event: PlantPlacementData): boolean {
+        if (this.plantToPlace == event.plant) {
+            this.plantsPlaced++;
+            return true;
+        }
+        return false;
+    }
+
+    tick(_delay: number): boolean { 
+        return false;
+    }
+}
+
+class DelayAfterCondition implements StartLevelCondition {
+    readonly condition: string = 'dodo:place_plant';
+    
+    readonly delay: number;
+
+    timeElapsed: number = 0;
+
+    readonly conditionBefore: StartLevelCondition;
+
+    checkedBefore = false;
+
+    constructor(condition: StartLevelCondition, delay: number) {
+        this.conditionBefore = condition;
+        this.delay = delay;
+    }
+
+    onPlant(event: PlantPlacementData): boolean {
+        if (this.checkedBefore) 
+            return false;
+        return this.conditionBefore.onPlant(event);
+    }
+
+    tick(delta: number) {
+        if (this.checkedBefore) {
+            this.timeElapsed += delta;
+            return this.timeElapsed >= this.delay;
+        }
+        return this.conditionBefore.tick(delta);
+    }
+    
+    check(_scene: Game): boolean {
+        if (this.checkedBefore) return this.timeElapsed >= this.delay;
+        if (this.conditionBefore.check(_scene)) {
+            this.checkedBefore = true;
+        }
+        return this.timeElapsed >= this.delay;
+    }
+}
+
 export interface Wave {
     time: number
     batch: ZombieGeneration[]
 }
+
+export interface PlantReward {
+    type: 'plant',
+    reward: PlantEntry
+}
+
+export type RewardData = PlantReward;
 
 export interface Level {
     startingSuns: number
@@ -20,6 +107,8 @@ export interface Level {
     spacing: number
     lanes: LaneType[]
     waves: Wave[]
+    startLevelConditions?: StartLevelCondition[]
+    reward?: RewardData
 }
 
 export enum LaneType {
@@ -29,9 +118,16 @@ export enum LaneType {
 
 export const LEVELS_REGISTRY = new Registry<Level>();
 LEVELS_REGISTRY.add('dodo:level_1-1', {
+    reward: {
+        type: 'plant',
+        reward: notNullOrUndefined(PLANTS_REGISTRY.get('dodo:sunflower')),
+    },
     startingSuns: 150,
     fixedSeeds: [
         notNullOrUndefined(PLANTS_REGISTRY.get('dodo:peashooter')),
+    ],
+    startLevelConditions: [
+        new DelayAfterCondition(new PlacePlantCondition(notNullOrUndefined(PLANTS_REGISTRY.get('dodo:peashooter')), 2), 5000),
     ],
     spacing: 10,
     lanes: [
@@ -49,19 +145,19 @@ LEVELS_REGISTRY.add('dodo:level_1-1', {
             ],
         },
         {
+            time: 10000,
+            batch: [
+                { selectLane: () => 2, zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
+            time: 20000,
+            batch: [
+                { selectLane: () => 2, zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
             time: 30000,
-            batch: [
-                { selectLane: () => 2, zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
-            ],
-        },
-        {
-            time: 60000,
-            batch: [
-                { selectLane: () => 2, zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
-            ],
-        },
-        {
-            time: 90000,
             batch: [
                 { selectLane: () => 2, zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
                 { selectLane: () => 2, zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
@@ -70,3 +166,65 @@ LEVELS_REGISTRY.add('dodo:level_1-1', {
     ],
 });
 
+LEVELS_REGISTRY.add('dodo:level_1-2', {
+    startingSuns: 50,
+    fixedSeeds: [
+        notNullOrUndefined(PLANTS_REGISTRY.get('dodo:peashooter')),
+        notNullOrUndefined(PLANTS_REGISTRY.get('dodo:sunflower')),
+    ],
+    startLevelConditions: [
+        new DelayAfterCondition(new PlacePlantCondition(notNullOrUndefined(PLANTS_REGISTRY.get('dodo:sunflower')), 3), 16000),
+    ],
+    spacing: 10,
+    lanes: [
+        LaneType.Ground,
+        LaneType.Grass,
+        LaneType.Grass,
+        LaneType.Grass,
+        LaneType.Ground,
+    ],
+    waves: [
+        {
+            time: 0,
+            batch: [
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
+            time: 30000,
+            batch: [
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
+            time: 60000,
+            batch: [
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
+            time: 90000,
+            batch: [
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
+            time: 120000,
+            batch: [
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+        {
+            time: 150000,
+            batch: [
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+                { selectLane: () => randomInt(1, 4), zombie: notNullOrUndefined(ZOMBIES_REGISTRY.get('dodo:zombie')) },
+            ],
+        },
+    ],
+});
